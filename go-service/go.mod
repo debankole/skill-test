d80cb751a76777c9da7cb1c649ce41aee8b5a7ac
bas <bas@iota.org>
2025-07-02 00:38:16 +0300

    Improve testing

--- go-service/go.mod
+++ go-service/go.mod
@@ -5,14 +5,18 @@ go 1.24.2
 require (
 	github.com/go-chi/chi/v5 v5.2.2
 	github.com/joho/godotenv v1.5.1
+	github.com/ledongthuc/pdf v0.0.0-20250511090121-5959a4027728
 	github.com/phpdave11/gofpdf v1.4.3
 	github.com/sirupsen/logrus v1.9.3
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.10.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- go-service/main_test.go
+++ go-service/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"io"
 	"net/http"
 	"os"
@@ -8,6 +9,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/ledongthuc/pdf"
 	"github.com/stretchr/testify/require"
 )
 
@@ -37,4 +39,34 @@ func TestIntegration_StudentReportPDF(t *testing.T) {
 	require.Equal(t, http.StatusOK, resp.StatusCode)
 	require.Equal(t, "application/pdf", resp.Header.Get("Content-Type"))
 	require.True(t, len(body) > 100, "PDF body should not be empty")
+
+	// --- PDF content check ---
+	text, err := extractTextFromBytes(body)
+	require.NoError(t, err)
+
+	require.Contains(t, text, "John Doe")
+	require.Contains(t, text, "john.doe@example.com")
+	require.Contains(t, text, "10")
+	require.Contains(t, text, "A")
+}
+
+func extractTextFromBytes(pdfData []byte) (string, error) {
+	reader := bytes.NewReader(pdfData)
+	pdfReader, err := pdf.NewReader(reader, int64(len(pdfData)))
+	if err != nil {
+		return "", err
+	}
+
+	var buf bytes.Buffer
+	totalPage := pdfReader.NumPage()
+	for pageIndex := 1; pageIndex <= totalPage; pageIndex++ {
+		page := pdfReader.Page(pageIndex)
+		content, err := page.GetPlainText(nil)
+		if err != nil {
+			return "", err
+		}
+		buf.WriteString(content)
+	}
+
+	return buf.String(), nil
 }
